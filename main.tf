8d0634b9a5092efa059af14fd28beeaea35770f3
QBY-MarkusMaring <106068259+QBY-MarkusMaring@users.noreply.github.com>
2023-02-14 11:22:21 +0100

    lower case workspace_name
    If the workspace name contains uppercase letters they are converted to lower case during resource deployment. Every apply would then redeploy the resource again.

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "azurerm_log_analytics_solution" "VMInsights" {
   location              = var.log_analytics_workspace.location
   resource_group_name   = var.log_analytics_workspace.resource_group_name
   workspace_resource_id = var.log_analytics_workspace.id
-  workspace_name        = var.log_analytics_workspace.name
+  workspace_name        = lower(var.log_analytics_workspace.name)
 
   plan {
     publisher = "Microsoft"
